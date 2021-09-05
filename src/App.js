449646c53fd64e6b09d33e579fc3b161b4a0c01f
awais512 <raza.awais25@gmail.com>
2021-09-05 21:19:19 +0500

    Complete Ecommerce App Template Using React and  Styled Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import ProductList from './pages/ProductList';
+import Announcement from './components/Announcement';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Product from './pages/Product';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Cart from './pages/Cart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Announcement />
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/products' component={ProductList} />
+        <Route exact path='/products/:id' component={Product} />
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/cart' component={Cart} />
+      </Switch>
+      <Footer />
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Categories from '../components/Categories';
+import Newsletter from '../components/Newsletter';
+import Products from '../components/Products';
+import Slider from '../components/Slider';
+
+const Home = () => {
+  return (
+    <div>
+      {/* <Announcement />
+      <Navbar /> */}
+      <Slider />
+      <Categories />
+      <Products />
+      <Newsletter />
+    </div>
+  );
+};
+
+export default Home;
